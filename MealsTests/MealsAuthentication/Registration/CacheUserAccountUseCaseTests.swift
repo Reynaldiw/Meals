a120dc413436fcb0a8868fdd3e0039624d9669f4
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-10-19 19:49:07 +0700

    `register` command, does not request to insert user account into cache on retrieval cache user account error.

--- MealsTests/MealsAuthentication/Registration/CacheUserAccountUseCaseTests.swift
+++ MealsTests/MealsAuthentication/Registration/CacheUserAccountUseCaseTests.swift
@@ -7,9 +7,36 @@
 
 import XCTest
 
+struct StoredUserAccount {}
+
+protocol UserAccountStore {
+    func retrieve() throws -> [StoredUserAccount]
+    func insert(_ userAccount: StoredUserAccount) throws
+}
+
+struct RegistrationUserAccount {
+    private let fullname: String
+    private let username: String
+    private let password: String
+    
+    init(fullname: String, username: String, password: String) {
+        self.fullname = fullname
+        self.username = username
+        self.password = password
+    }
+}
+
 final class RegistrationUserAccountService {
     
-    init(store: Any) {}
+    private let store: UserAccountStore
+    
+    init(store: UserAccountStore) {
+        self.store = store
+    }
+    
+    func register(_ userAccount: RegistrationUserAccount) {
+        _ = try? store.retrieve()
+    }
 }
 
 final class CacheUserAccountUseCaseTests: XCTestCase {
@@ -21,10 +48,36 @@ final class CacheUserAccountUseCaseTests: XCTestCase {
         XCTAssertEqual(store.messages, [])
     }
     
+    func test_register_doesNotRequestInsertionOnRetrievalError() {
+        let userAccount = RegistrationUserAccount(
+            fullname: "any-fullname",
+            username: "any-username",
+            password: "any-password")
+        let store = UserAccountStoreSpy()
+        let sut = RegistrationUserAccountService(store: store)
+        
+        sut.register(userAccount)
+        
+        XCTAssertEqual(store.messages, [.retrieveCacheUserAccount])
+    }
+    
     //MARK: - Helpers
     
-    private class UserAccountStoreSpy {
+    private class UserAccountStoreSpy: UserAccountStore {
         
-        private(set) var messages: [String] = []
+        enum Message: Equatable {
+            case retrieveCacheUserAccount
+        }
+        
+        private(set) var messages: [Message] = []
+        
+        private var retrievalResult: Result<[StoredUserAccount], Error>?
+        
+        func retrieve() throws -> [StoredUserAccount] {
+            messages.append(.retrieveCacheUserAccount)
+            return try retrievalResult?.get() ?? []
+        }
+        
+        func insert(_ userAccount: StoredUserAccount) throws {}
     }
 }

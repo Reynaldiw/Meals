e7a23fdc115c53436a4acb4a22ee0f09f6830fb8
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-10-19 23:57:07 +0700

    Create `AuthenticateUserAccountMapper` to map `AuthenticateUserAccount` into `base-64 encoded string` and vice versa.

---
+++ Meals/MealsAuthentication/Login/Store/AuthenticateUserAccountMapper.swift
@@ -0,0 +1,47 @@
+//
+//  AuthenticateUserAccountMapper.swift
+//  Meals
+//
+//  Created by Reynaldi on 19/10/23.
+//
+
+import Foundation
+
+public final class AuthenticateUserAccountMapper {
+    
+    private struct CodableAuthenticateUserAccount: Codable {
+        public let id: UUID
+        public let fullname: String
+        public let username: String
+        public let createdAt: String
+        
+        public init(id: UUID, fullname: String, username: String, createdAt: String) {
+            self.id = id
+            self.fullname = fullname
+            self.username = username
+            self.createdAt = createdAt
+        }
+        
+        var model: AuthenticateUserAccount {
+            AuthenticateUserAccount(id: id, fullname: fullname, username: username, createdAt: createdAt)
+        }
+    }
+    
+    private init() {}
+    
+    /// Mapping model `AuthenticateUserAccount` into `Base64-Encoded String`
+    private static func map(_ model: AuthenticateUserAccount) throws -> String {
+        let codableModel = CodableAuthenticateUserAccount(
+            id: model.id,
+            fullname: model.fullname,
+            username: model.username,
+            createdAt: model.createdAt)
+        return try JSONEncoder().encode(codableModel).base64EncodedString()
+    }
+    
+    /// Mapping model `Base64-Encoded String`  into `AuthenticateUserAccount`
+    private static func map(base64EncodedValue value: String) throws -> AuthenticateUserAccount? {
+        guard let data = Data(base64Encoded: value) else { return nil }
+        return try JSONDecoder().decode(CodableAuthenticateUserAccount.self, from: data).model
+    }
+}

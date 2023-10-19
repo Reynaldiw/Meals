7e14813de0b7ceac7134db83f9f904f1363240e6
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-10-19 18:20:26 +0700

    Record and assert registration UI page for not all required fields is filled.

--- MealsTests/MealsiOS/Registration UI/RegistrationSnapshotTests.swift
+++ MealsTests/MealsiOS/Registration UI/RegistrationSnapshotTests.swift
@@ -16,6 +16,13 @@ final class RegistrationSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(configuration: .iPhone13(style: .light)), named: "REGISTRATION_INITIAL_SETUP_light")
     }
     
+    func test_registrationWithNotAllOfRequiredFieldsIsFilled() {
+        let sut = makeSUT()
+        sut.fillAllRequiredFields(false)
+        
+        assert(snapshot: sut.snapshot(configuration: .iPhone13(style: .light)), named: "REGISTRATION_NOT_ALL_REQUIRED_FIELDS_FILLED_light")
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT() -> RegistrationViewController {
@@ -25,5 +32,16 @@ final class RegistrationSnapshotTests: XCTestCase {
         controller.loadViewIfNeeded()
         
         return controller
+    }
+}
+
+private extension RegistrationViewController {
+    func fillAllRequiredFields(_ isAllFilled: Bool) {
+        fullnameField.text = "Test Fullname"
+        
+        if isAllFilled {
+            usernameField.text = "Test Username"
+            passwordField.text = "Test Password"
+        }
     }
 }

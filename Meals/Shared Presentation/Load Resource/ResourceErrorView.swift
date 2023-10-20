f86a9ea2551600f0c8d47220bd1f68b75aac053b
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-10-21 06:42:09 +0700

    Create `LoadResouce` presenter, as an shared helper presenter to bridge the view to update state when load resource.

---
+++ Meals/Shared Presentation/Load Resource/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  Meals
+//
+//  Created by Reynaldi on 21/10/23.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}

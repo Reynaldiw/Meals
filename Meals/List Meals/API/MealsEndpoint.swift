115bfb6833f2a7377684c40f0fa0c0d290798ba4
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-10-21 06:28:37 +0700

    Create `MealsEndpoint` to get the URL for request to API

---
+++ Meals/List Meals/API/MealsEndpoint.swift
@@ -0,0 +1,27 @@
+//
+//  MealsEndpoint.swift
+//  Meals
+//
+//  Created by Reynaldi on 21/10/23.
+//
+
+import Foundation
+
+public enum MealsEndpoint {
+    case get
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case .get:
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "json/v1/1/search.php"
+            components.queryItems = [
+                URLQueryItem(name: "f", value: "a")
+            ].compactMap { $0 }
+            
+            return components.url!
+        }
+    }
+}

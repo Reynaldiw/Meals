5b1b99ca48667a72660b05b84f6e051254e38f03
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-10-21 05:47:36 +0700

    Create domain data model for List Meal

---
+++ Meals/List Meals/Feature/MealItem.swift
@@ -0,0 +1,22 @@
+//
+//  MealItem.swift
+//  Meals
+//
+//  Created by Reynaldi on 21/10/23.
+//
+
+import Foundation
+
+public struct MealItem {
+    public let id: String
+    public let imageURL: URL
+    public let name: String
+    public let category: String
+    
+    public init(id: String, imageURL: URL, name: String, category: String) {
+        self.id = id
+        self.imageURL = imageURL
+        self.name = name
+        self.category = category
+    }
+}
